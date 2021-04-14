446f1a0bdf424c1bb5f505e8ca99ce230cbb1e83
Daniel Callus <danielcallus@gmail.com>
2021-04-14 16:35:58 +0100

    finished extentions -css

--- models/order.py
+++ models/order.py
@@ -7,6 +7,6 @@ class Order:
         self.pizza_type = pizza_type
         self.base_type = base_type
         self.size = size
-        self.description = f"{self.quantity} x {self.size} {self.pizza_type} ({self.base_type}) ordered at ({self.order_date})"
+        # self.description = f"{self.quantity} x {self.size} {self.pizza_type} ({self.base_type}) ordered at ({self.order_date})"
 
         """Dave: 2 x extra large Meat Feast (deep pan) ordered at order_date"""
