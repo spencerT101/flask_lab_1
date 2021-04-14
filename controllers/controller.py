9a3e5f419a228b66a4e8938a4ed8e0c609e67019
Daniel Callus <danielcallus@gmail.com>
2021-04-14 15:49:00 +0100

    one last part of extension1 to do

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,7 @@ from models.customer_orders import customers
 @app.route('/customers')
 def index():
     return render_template('index.html', title='Pizza joint', customers=customers)
+
+@app.route('/customers/<id>')
+def show(id):
+    return render_template('order.html', title='Customer Order', customer=customers[int(id)])
